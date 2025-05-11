4dae01c0ee143c367854564592efb6b096bd7080
Panagiotis Panopoulos (Pan Pan) <2014pan@gmail.com>
2025-05-11 08:52:50 +0300

    Update sparkethos_checker.py

--- api/sparkethos_checker.py
+++ api/sparkethos_checker.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/api/sparkethos_checker")
 def sparkethos_checker():
     try:
-        r = requests.get("https://sparkethos-guide.netlify.app/sparkethos_py.txt")
+        r = requests.get("https://sparkethos.vercel.app/sparkethos_py.txt")
         r.raise_for_status()
         return Response(r.text, mimetype='text/plain')
     except Exception as e:
