1c86cc0606e58c513f21699785a8ab8ae8869e64
Panagiotis Panopoulos (Pan Pan) <2014pan@gmail.com>
2025-05-11 08:02:12 +0300

    Update sparkethos_checker.py

--- netlify/functions/sparkethos_checker.py
+++ netlify/functions/sparkethos_checker.py
@@ -1,1 +1,19 @@
 
+import urllib.request
+
+def handler(event, context):
+    url = "https://sparkethos-guide.netlify.app/sparkethos_py.txt"
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            content = response.read().decode("utf-8")
+            return {
+                "statusCode": 200,
+                "headers": {"Content-Type": "text/plain"},
+                "body": content
+            }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": f"Σφάλμα: {str(e)}"
+        }
