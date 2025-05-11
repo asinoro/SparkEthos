6611406b30f4c0285ab4a48567ce81ba9dba654f
Panagiotis Panopoulos (Pan Pan) <2014pan@gmail.com>
2025-05-11 11:36:44 +0300

    Update hello.py

--- api/hello.py
+++ api/hello.py
@@ -1,1 +1,10 @@
+import json
 
+def handler(request):
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"message": "Hello from Vercel Functions!"}),
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    }
